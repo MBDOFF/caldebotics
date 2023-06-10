d8e6abfa1bc928065317e8ffd8581466fa1040c5
101happy <marinela.miclau@gmail.com>
2023-06-10 18:30:46 -0400

    added speedometer

---
+++ meteo/generate.py
@@ -0,0 +1,37 @@
+import csv
+import random
+import time
+
+MAX_DATA_ENTRIES = 50
+
+while True:
+    # Generate random data
+    temperature = random.randint(-20, 40)
+    humidity = random.randint(0, 100)
+
+    # Read existing data from the CSV file
+    existing_data = []
+    try:
+        with open('data.txt', 'r') as file:
+            reader = csv.reader(file)
+            next(reader)  # Skip header row
+            for row in reader:
+                existing_data.append(row)
+    except FileNotFoundError:
+        pass
+
+    # Remove excess data if more than MAX_DATA_ENTRIES
+    if len(existing_data) >= MAX_DATA_ENTRIES:
+        existing_data = existing_data[-(MAX_DATA_ENTRIES - 1):]
+
+    # Append the new data
+    existing_data.append([temperature, humidity])
+
+    # Write updated data to the CSV file
+    with open('data.txt', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['temperature', 'humidity'])
+        writer.writerows(existing_data)
+
+    # Wait for 2 seconds
+    time.sleep(2)
---
+++ meteo/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const fs = require('fs');
+const csv = require('csv-parser');
+const path = require('path');
+
+const app = express();
+
+app.get('/data', (req, res) => {
+  const data = [];
+
+  fs.createReadStream('data.txt')
+    .pipe(csv())
+    .on('data', (row) => {
+      data.push(row);
+    })
+    .on('end', () => {
+      const lastEntry = data[data.length - 1];
+      const jsonData = {
+        temperature: lastEntry.temperature,
+        humidity: lastEntry.humidity,
+      };
+
+      res.json(jsonData);
+    });
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
